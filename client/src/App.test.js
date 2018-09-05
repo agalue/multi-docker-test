4a8cd29e94bb61685c961960b36eff787e556a21
Alejandro Galue <agalue@opennms.org>
2018-09-05 15:57:15 -0400

    Refactor the test to make it run with mock-axios.

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,7 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+
 it('renders without crashing', () => {
+  var mock = new MockAdapter(axios);
+  mock.onGet('/api/values/current').reply(200, { '5': '8' });
+  mock.onGet('/api/values/all').reply(200, [{ 'number': '5' }]);
+
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
